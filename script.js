b2380edb58f6eac24a00149865faf1d0c6928720
soyalkhan <soyalkhan76@gmail.com>
2024-03-03 12:51:20 +0530

    complete

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 let btn = document.querySelector(".btn");
 
-async function generateJokeRandom(){
-   const url = await fetch("https://official-joke-api.appspot.com/random_joke");
-   try {
-    const result = await  url.json();
-    jokes.innerHTML = "Fetching....."
+async function generateJokeRandom() {
+  const url = await fetch("https://official-joke-api.appspot.com/random_joke");
+  try {
+    const result = await url.json();
+    jokes.innerHTML = "Fetching.....";
     console.log(url);
-    console.log(result); 
-    console.log("Id:",result.id);
-    console.log("Title:",result.punchline);
-    console.log("Content:",result.setup);
-    jokes.innerHTML = result.setup;
+    console.log(result);
+    console.log("Id:", result.id);
+    console.log("Title:", result.punchline);
+    console.log("Content:", result.setup);
     
-
-   } catch (error) {
+    jokes.innerHTML = result.setup;
+  } catch (error) {
     console.log(error);
-   }   
+  }
 }
-function randomNumber(){
-    let RandomNum = Math.floor(Math.random() * 10);
-    console.log(RandomNum);
+function randomNumber() {
+  let RandomNum = Math.floor(Math.random() * 10);
+  console.log(RandomNum);
 }
 
+btn.addEventListener("click", generateJokeRandom);
+btn.addEventListener("click", randomNumber);
 
-btn.addEventListener("click", generateJokeRandom)
-btn.addEventListener("click", randomNumber)
